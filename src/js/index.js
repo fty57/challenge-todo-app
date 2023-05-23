4a9b366603b2a9d12919221523e48ca0f23ef975
fty57 <fty.57@alu.ufc.br>
2023-05-23 11:18:26 -0300

    Adição e remoção de tarefas
    - Novos assets
    - Outro jeito de fazer os todo-items
    - As funções utilizadas
    - Ajustes no CSS

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,118 @@
-function addItem(event) {
-  event.preventDefault();
-  let text = document.getElementById('todo-input');
-  console.log(text.value);
+const inputBox = document.getElementById('todo-input');
+const todoContainer = document.getElementById('todo-container');
+
+function countRemainingTasks() {
+  var todoList = document.getElementById("todo-container");
+  var tasks = todoList.getElementsByTagName("li");
+  var remainingTasks = 0;
+
+  for (var i = 0; i < tasks.length; i++) {
+    if (!tasks[i].classList.contains("checked")) {
+      remainingTasks++;
+    }
+  }
+
+  return remainingTasks;
 }
+
+function updateItemsLeft() {
+  var itemsLeftDiv = document.querySelector(".items-left");
+  var remaining = countRemainingTasks();
+  itemsLeftDiv.textContent = remaining + " items left";
+}
+
+function addTodoItem() {
+  if (inputBox.value === '') {
+    alert('You must write something!');
+  }
+  else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    todoContainer.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  saveData();
+}
+
+function filterTasks(filterOption) {
+  var todoList = document.getElementById("todo-container");
+  var tasks = todoList.getElementsByTagName("li");
+
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+
+    if (filterOption === "active") {
+      if (task.classList.contains("checked")) {
+        task.classList.add("hidden");
+      } else {
+        task.classList.remove("hidden");
+      }
+    } else if (filterOption === "checked") {
+      if (task.classList.contains("checked")) {
+        task.classList.remove("hidden");
+      } else {
+        task.classList.add("hidden");
+      }
+    } else {
+      // Mostrar todas as tarefas
+      task.classList.remove("hidden");
+    }
+  }
+}
+
+var itemsStatuses = document.querySelector(".items-statuses");
+
+itemsStatuses.addEventListener("click", function (e) {
+  if (e.target.tagName === "SPAN") {
+    var filterOption = e.target.textContent.toLowerCase();
+    filterTasks(filterOption);
+  }
+});
+
+
+function clearCompletedTasks() {
+  var todoList = document.getElementById("todo-container");
+  var tasks = todoList.getElementsByTagName("li");
+
+  for (var i = tasks.length - 1; i >= 0; i--) {
+    var task = tasks[i];
+
+    if (task.classList.contains("checked")) {
+      task.remove();
+    }
+  }
+}
+
+var itemsClear = document.querySelector(".items-clear");
+
+itemsClear.addEventListener("click", function () {
+  clearCompletedTasks();
+});
+
+
+
+todoContainer.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    saveData();
+  }
+  else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove()
+    saveData();
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem('data', todoContainer.innerHTML);
+}
+
+function showTask() {
+  todoContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
+updateItemsLeft();
+setInterval(updateItemsLeft, 1000);
